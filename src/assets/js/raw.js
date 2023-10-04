87147af0a96d312085120de962ac8d9b78010536
ManiBHF <manibeh84@gmail.com>
2023-10-04 19:56:02 +0330

    new update

---
+++ src/assets/js/raw.js
@@ -0,0 +1,16 @@
+let tabs = document.querySelectorAll(".tabs button");
+let tabContents = document.querySelectorAll(".tab-content section");
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    tabContents.forEach((content) => {
+      content.classList.remove("active");
+    });
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    tabContents[index].classList.add("active");
+    tabs[index].classList.add("active");
+  });
+});
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1603px',
+    },
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('../images/social-media-bg.png')",
+      }
+    },
   },
   plugins: [],
 }
