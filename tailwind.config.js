f468d832cd936ebbb2239e9ca094ffae2367b7c7
ramtin behforouzi <ramtin.arm@gmail.com>
2023-10-08 03:19:09 +0330

    update index
    just footer update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,19 +4,24 @@ module.exports = {
   theme: {
     container: {
       center: true,
+      padding:{
+        'DEFAULT': '30px',
+        'md': '2rem',
+        '2xl': '6rem',
+      }
     },
     screens: {
       'sm': '640px',
       'md': '768px',
       'lg': '1024px',
       'xl': '1280px',
-      '2xl': '1603px',
+      '2xl': '1580px',
     },
     extend: {
       backgroundImage: {
         'hero-pattern': "url('../images/social-media-bg.png')",
         'hero-pattern-mb': "url('../images/Union.png')",
-        'hero-pattern-footer': "url('../images/footer.png')",
+        'hero-pattern-footer': "url('../images/bg-footer.png')",
         'hero-pattern-footer-mb': "url('../images/bg-foo-mb.png')",
       }
     },
