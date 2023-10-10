cb471cc75a21cfcd53c89ba533aeb757e14759b5
ManiBHF <manibeh84@gmail.com>
2023-10-10 18:40:05 +0330

    hello

--- src/assets/js/raw.js
+++ src/assets/js/raw.js
@@ -14,3 +14,83 @@ tabs.forEach((tab, index) => {
   });
 });
 
+
+function myFunction() {
+  let dots = document.getElementById("dots");
+  let moreText = document.getElementById("more");
+  let btnText = document.getElementById("myBtn-h");
+
+
+if (dots.style.display === "none") {
+  dots.style.display = "inline";
+  btnText.innerHTML = "Read more";
+  moreText.style.display = "none";
+} else {
+  // dots.style.display = "none";
+  btnText.style.display = "none"
+  // btnText.innerHTML = "Read less";
+  moreText.style.display = "inline";
+}
+}
+
+
+
+var modal = document.getElementById("myModal");
+
+
+// Get the button that opens the modal
+var btn1 = document.getElementById("myBtn");
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+
+// When the user clicks the button, open the modal
+btn1.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+
+// const buttonLabels = ["دکمه ۱", "دکمه ۲", "دکمه ۳", "دکمه ۴", "دکمه ۵", "دکمه ۶", "دکمه ۷", "دکمه ۸"];
+
+// const buttonsContainer = document.getElementById('buttons');
+// const modal = document.getElementById('modal');
+// const modalText = document.getElementById('modalText');
+// const closeModal = document.getElementById('closeModal');
+
+// buttonLabels.forEach((label, index) => {
+//     const button = document.createElement('button');
+//     button.textContent = label;
+//     buttonsContainer.appendChild(button);
+
+//     button.addEventListener('click', () => {
+//         modal.style.display = 'block';
+//         modalText.textContent = ${index + 1} ;
+//     });
+// });
+
+// closeModal.addEventListener('click', () => {
+//     modal.style.display = 'none';
+// });
+
+// window.addEventListener('click', (event) => {
+//     if (event.target === modal) {
+//         modal.style.display = 'none';
+//     }
+// });
+
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
   theme: {
     container: {
       center: true,
+      padding:{
+        'DEFAULT': '30px',
+        'md': '2rem',
+        '2xl': '6rem',
+      }
     },
     screens: {
       'sm': '640px',
@@ -18,6 +23,13 @@ module.exports = {
         'hero-pattern-mb': "url('../images/Union.png')",
         'hero-pattern-footer': "url('../images/footer.png')",
         'hero-pattern-footer-mb': "url('../images/bg-foo-mb.png')",
+        'hero-pattern-tn': "url('../images/tournament-top.png')",
+        'hero-pattern-st': "url('../images/Subtract.png')",
+        'btn-tr': "url('../images/btn-tr.png')",
+        'header-tor': "url('../images/header-tour.png')",
+        'header-tor-mb': "url('../images/header-mb-tour.png')",
+        'main-tr': "url('../images/main-tr.png')",
+
       }
     },
   },
